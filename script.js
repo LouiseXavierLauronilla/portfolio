1dab82127f1c0121eca8ba0a867f78882b6c1513
LouiseXavierLauronilla <louisexavierlauronilla@gmail.com>
2025-10-15 20:58:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeColor() {
+    const box = document.getElementById("colorBox");
+    const colors = ["blue", "red", "yellow", "orange", "green", "purple", "pink"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    box.style.backgroundColor = randomColor;
+}

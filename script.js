1e7d47ed3c2023abee683903c13754c66a54023a
LouiseXavierLauronilla <louisexavierlauronilla@gmail.com>
2025-10-15 22:21:34 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("changeColorBtn").addEventListener("click", function() {
-    const colors = ["lightcoral", "lightgreen", "lightpink", "lightyellow", "lightgray", "skyblue"];
-    const box = document.getElementById("colorBox");
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    box.style.backgroundColor = randomColor;
-});

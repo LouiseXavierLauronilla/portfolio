5894708c53c45454f66ad62faf5662efd424b07a
LouiseXavierLauronilla <louisexavierlauronilla@gmail.com>
2025-10-15 21:49:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function changeColor() {
+document.getElementById("changeColorBtn").addEventListener("click", function() {
+    const colors = ["lightcoral", "lightgreen", "lightpink", "lightyellow", "lightgray", "skyblue"];
     const box = document.getElementById("colorBox");
-    const colors = ["blue", "red", "yellow", "orange", "green", "purple", "pink"];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     box.style.backgroundColor = randomColor;
-}
+});
